392608568452f2770d0e05c88663ccfd54a0832e
emazupp <zuppardo.emanuele2001@gmail.com>
2024-11-28 16:50:31 +0100

    css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,17 +2,20 @@ export default function Main() {
   return (
     <>
       <div className="container">
-        <div className="card">
-          <div className="card-img">
-            <img src="https://picsum.photos/600/400?grayscale" alt="img" />
-          </div>
-          <div className="card-body">
-            <h3>Titolo del post</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
-              odio amet eveniet consectetur ducimus dolorum qui ipsa quia
-              dolores vitae!
-            </p>
+        <div className="card-container">
+          <div className="card">
+            <div className="card-img">
+              <img src="https://picsum.photos/600/400" alt="img" />
+            </div>
+            <div className="card-body">
+              <h3>Titolo del post</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+                explicabo illo commodi assumenda quos quas dolores quod fuga.
+                Soluta quos obcaecati labore iusto neque recusandae fugiat eaque
+                debitis praesentium veniam?
+              </p>
+            </div>
           </div>
         </div>
       </div>
