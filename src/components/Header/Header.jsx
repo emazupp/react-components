090549666cfdb766fa3013ee0480272de8af9cc2
emazupp <zuppardo.emanuele2001@gmail.com>
2024-11-28 17:19:07 +0100

    bonus header module css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
+import style from "./Header.module.css";
 export default function Header() {
   return (
     <>
-      <div className="container header">
+      <div className={`container ${style.header}`}>
         <h1 className="header-title">Il mio blog</h1>
       </div>
     </>
